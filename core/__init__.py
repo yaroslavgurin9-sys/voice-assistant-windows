5d4a4c4fa6841342cb5b863134ad09c1bf47a371
yaroslavgurin9-sys <yaroslav.gurin9@gmail.com>
2026-02-13 18:06:23 +0300

    Add core package init

--- core/__init__.py
+++ core/__init__.py
@@ -1,21 +1,12 @@
-from .logger import app_logger, error_logger, log_command, log_error
-from .audio_input import AudioCapture, VoiceActivityDetector
-from .wake_word import WakeWordDetector
-from .stt_engine import VoskSTT, SpeechToTextPipeline
-from .tts_engine import TextToSpeechEngine, tts_engine
-from .command_router import CommandRouter
-from .system_controller import SystemController, ApplicationLauncher
-from .screen_analyzer import ScreenAnalyzer
-from .ai_chat import AIChat
+"""Core modules for Voice Assistant"""
 
 __all__ = [
-    'app_logger', 'error_logger', 'log_command', 'log_error',
-    'AudioCapture', 'VoiceActivityDetector',
+    'AudioCapture',
+    'VoiceActivityDetector',
     'WakeWordDetector',
-    'VoskSTT', 'SpeechToTextPipeline',
-    'TextToSpeechEngine', 'tts_engine',
+    'SpeechToTextPipeline',
+    'TextToSpeechEngine',
     'CommandRouter',
-    'SystemController', 'ApplicationLauncher',
-    'ScreenAnalyzer',
-    'AIChat',
+    'app_logger',
+    'error_logger',
 ]
